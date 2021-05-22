a0963befcfc0e75efc6945af78cbbe7c670af801
larissapaiva <paiva.larissa.silva@gmail.com>
2021-05-21 21:15:16 -0300

    super lanche

--- src/app/paginas/dashboard/dashboard.page.ts
+++ src/app/paginas/dashboard/dashboard.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { ProdutosService } from '../../servicos/produtos.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
-
-  constructor() { }
+  infos: any;
+  constructor( private servico: ProdutosService) { }
 
   ngOnInit() {
+    this.servico.listar().subscribe(data => {
+      this.infos=data.map(e=> {
+        return { 
+          id:e.payload.doc.id,
+          nome:e.payload.doc.data()['pnome'],
+          valor:e.payload.doc.data()['pvalor'],
+          caminho:e.payload.doc.data()['caminho']
+        }
+       
+      })
+      console.log(this.infos);
+    });
   }
 
 }
